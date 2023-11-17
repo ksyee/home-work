f665e3caa01d35cee70781a7e8b318d2d234a37e
ksyee <tjssud0647@gmail.com>
2023-11-17 12:52:06 +0900

    mission-03 markup, style complete

---
+++ mission-03/js/index.js
@@ -0,0 +1,11 @@
+// const btn = document.querySelectorAll('.btn');
+
+// btn.forEach((btn) => {
+//   btn.addEventListener('focus', () => {
+//     btn.classList.add('is--focus');
+//   });
+
+//   btn.addEventListener('blur', () => {
+//     btn.classList.remove('is--focus');
+//   });
+// });
